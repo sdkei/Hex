2807d4af4deeaa7c86d4cb7ebc8a7531bf024a88
sdkei <sdkei0xb@gmail.com>
2018-10-22 22:51:03 +0900

    feat: add PseudoHexH class and it's test.

--- src/main/kotlin/io/github/sdkei/hex/core/PseudoHexH.kt
+++ src/main/kotlin/io/github/sdkei/hex/core/PseudoHexH.kt
@@ -1,27 +1,30 @@
 package io.github.sdkei.hex.core
 
 /**
- * Absolute logical location of a hex on pseudo hex plane represented by 2-dimensional array.
+ * Absolute logical location of a hex,
+ * which aligns horizontally,
+ * on pseudo hex plane represented by 2-dimensional array.
  *
  * ```
+ * ↓y x→
  * 0  |0|1|2|3|_
  * 1  _|0|1|2|3|
  * 2  |0|1|2|3|_
  * 3  _|0|1|2|3|
  * ```
  */
-data class PseudoHexVectorH(
+data class PseudoHexH(
         val x: Int,
         val y: Int
 )
 
-fun PseudoHexVectorH.toHexVector(): HexVectorH =
+fun PseudoHexH.toHexVector(): HexVectorH =
         HexVectorH.UNIT_3 * (x - y / 2) +
                 HexVectorH.UNIT_5 * y
 
-fun HexVectorH.toPseudoHexVector(): PseudoHexVectorH {
+fun HexVectorH.toPseudoHex(): PseudoHexH {
     val (_3, _5) = elementsOf(SideH._3, SideH._5)
-    return PseudoHexVectorH(
+    return PseudoHexH(
             _3 + _5 / 2,
             _5)
 }
---
+++ src/test/kotlin/io/github/sdkei/hex/core/PseudoHexHTest.kt
@@ -0,0 +1,54 @@
+package io.github.sdkei.hex.core
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PseudoHexHTest {
+    @Test
+    fun test_toHexVector() {
+        assertEquals(HexVectorH.UNIT_3 * 0 + HexVectorH.UNIT_5 * 0,
+                PseudoHexH(0, 0).toHexVector())
+        assertEquals(HexVectorH.UNIT_3 * 1 + HexVectorH.UNIT_5 * 0,
+                PseudoHexH(1, 0).toHexVector())
+        assertEquals(HexVectorH.UNIT_3 * 2 + HexVectorH.UNIT_5 * 0,
+                PseudoHexH(2, 0).toHexVector())
+
+        assertEquals(HexVectorH.UNIT_3 * 0 + HexVectorH.UNIT_5 * 1,
+                PseudoHexH(0, 1).toHexVector())
+        assertEquals(HexVectorH.UNIT_3 * 1 + HexVectorH.UNIT_5 * 1,
+                PseudoHexH(1, 1).toHexVector())
+        assertEquals(HexVectorH.UNIT_3 * 2 + HexVectorH.UNIT_5 * 1,
+                PseudoHexH(2, 1).toHexVector())
+
+        assertEquals(HexVectorH.UNIT_3 * -1 + HexVectorH.UNIT_5 * 2,
+                PseudoHexH(0, 2).toHexVector())
+        assertEquals(HexVectorH.UNIT_3 * 0 + HexVectorH.UNIT_5 * 2,
+                PseudoHexH(1, 2).toHexVector())
+        assertEquals(HexVectorH.UNIT_3 * 1 + HexVectorH.UNIT_5 * 2,
+                PseudoHexH(2, 2).toHexVector())
+    }
+
+    @Test
+    fun test_toPseudoHexVectorH() {
+        assertEquals(PseudoHexH(0, 0),
+                (HexVectorH.UNIT_3 * 0 + HexVectorH.UNIT_5 * 0).toPseudoHex())
+        assertEquals(PseudoHexH(1, 0),
+                (HexVectorH.UNIT_3 * 1 + HexVectorH.UNIT_5 * 0).toPseudoHex())
+        assertEquals(PseudoHexH(2, 0),
+                (HexVectorH.UNIT_3 * 2 + HexVectorH.UNIT_5 * 0).toPseudoHex())
+
+        assertEquals(PseudoHexH(0, 1),
+                (HexVectorH.UNIT_3 * 0 + HexVectorH.UNIT_5 * 1).toPseudoHex())
+        assertEquals(PseudoHexH(1, 1),
+                (HexVectorH.UNIT_3 * 1 + HexVectorH.UNIT_5 * 1).toPseudoHex())
+        assertEquals(PseudoHexH(2, 1),
+                (HexVectorH.UNIT_3 * 2 + HexVectorH.UNIT_5 * 1).toPseudoHex())
+
+        assertEquals(PseudoHexH(1, 1),
+                (HexVectorH.UNIT_3 * 0 + HexVectorH.UNIT_5 * 2).toPseudoHex())
+        assertEquals(PseudoHexH(2, 1),
+                (HexVectorH.UNIT_3 * 1 + HexVectorH.UNIT_5 * 2).toPseudoHex())
+        assertEquals(PseudoHexH(3, 1),
+                (HexVectorH.UNIT_3 * 2 + HexVectorH.UNIT_5 * 2).toPseudoHex())
+    }
+}
