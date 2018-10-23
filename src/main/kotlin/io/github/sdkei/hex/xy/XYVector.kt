1d7ae40e92be9f465abf2f8b90d3bf530f138ad0
sdkei <sdkei0xb@gmail.com>
2018-10-23 20:16:07 +0900

    refactor: merge ApexToXYVector.kt into XYVector.kt .

--- src/main/kotlin/io/github/sdkei/hex/xy/XYVector.kt
+++ src/main/kotlin/io/github/sdkei/hex/xy/XYVector.kt
@@ -1,5 +1,16 @@
 package io.github.sdkei.hex.xy
 
+import io.github.sdkei.hex.core.ApexH
+import kotlin.math.sqrt
+
+private val DISTANCE_BETWEEN_ADJACENT_HEXES: Double = 1.0
+private val DISTANCE_BETWEEN_CENTER_AND_SIDE: Double = DISTANCE_BETWEEN_ADJACENT_HEXES / 2.0
+private val DISTANCE_BETWEEN_OPPOSITE_SIDE = DISTANCE_BETWEEN_ADJACENT_HEXES
+private val DISTANCE_BETWEEN_OPPOSITE_APEXES: Double = DISTANCE_BETWEEN_OPPOSITE_SIDE / sqrt(3.0) * 2.0
+private val DISTANCE_BETWEEN_CENTER_AND_APEX: Double = DISTANCE_BETWEEN_OPPOSITE_APEXES / 2.0
+private val DISTANCE_BETWEEN_APEXES: Double = DISTANCE_BETWEEN_OPPOSITE_SIDE / sqrt(3.0) * 1.0
+private val DISTANCE_BETWEEN_MIDDLE_OF_SIDE_AND_APEX = DISTANCE_BETWEEN_APEXES / 2.0
+
 /**
  * A vector in XY 2-dimensional rectangular coordinates.
  *
@@ -26,3 +37,17 @@ operator fun XYVector.times(scalar: Double) =
 
 operator fun XYVector.div(scalar: Double) =
         XYVector(x / scalar, y / scalar)
+
+/**
+ * Returns [XYVector] which is from center of a hex to `this` apex.
+ */
+fun ApexH.toXYVector(): XYVector {
+    return when (this) {
+        ApexH._0 -> XYVector(0.0, -DISTANCE_BETWEEN_CENTER_AND_APEX)
+        ApexH._2 -> XYVector(+DISTANCE_BETWEEN_CENTER_AND_SIDE, -DISTANCE_BETWEEN_MIDDLE_OF_SIDE_AND_APEX)
+        ApexH._4 -> XYVector(+DISTANCE_BETWEEN_CENTER_AND_SIDE, +DISTANCE_BETWEEN_MIDDLE_OF_SIDE_AND_APEX)
+        ApexH._6 -> XYVector(0.0, +DISTANCE_BETWEEN_CENTER_AND_APEX)
+        ApexH._8 -> XYVector(-DISTANCE_BETWEEN_CENTER_AND_SIDE, +DISTANCE_BETWEEN_MIDDLE_OF_SIDE_AND_APEX)
+        ApexH._10 -> XYVector(-DISTANCE_BETWEEN_CENTER_AND_SIDE, -DISTANCE_BETWEEN_MIDDLE_OF_SIDE_AND_APEX)
+    }
+}
